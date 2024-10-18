d24c1a5f96963195b64d38c64ed4b5f1568645bd
HecBarLu <hectorbarrioslujan@gmail.com>
2024-10-18 13:11:11 +0200

    Creadas clases NodoLista y Lista pero sin nada dentro salvo algun metodo

---
+++ PECLRiofrioOrtegaRoberto/Lista.hpp
@@ -0,0 +1,18 @@
+#ifndef LISTA_HPP
+#define LISTA_HPP
+
+class Lista
+{
+public:
+	Lista();
+	~Lista();
+	void mostrar();
+	int verPrimero();
+	void insertar(Proceso v);
+	void eliminar();
+
+private:
+	
+};
+
+#endif // LISTA_HPP
---
+++ PECLRiofrioOrtegaRoberto/NodoLista.hpp
@@ -0,0 +1,13 @@
+#ifndef NODOLISTA_HPP
+#define NODOLISTA_HPP
+
+class NodoLista
+{
+public:
+	NodoLista();
+	~NodoLista();
+	
+
+};
+
+#endif // NODOLISTA_HPP
